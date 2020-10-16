ebdc2f0226a9fa8a97de58c8d99534c5080fe8d0
cedrik <cedriksaraiva@gmail.com>
2020-10-16 12:57:33 +0200

    Cédrik - JS

---
+++ index.js
@@ -0,0 +1,14 @@
+var buttonBack = document.getElementById("buttonBack");
+buttonBack.addEventListener("click", function(){
+    console.log("clic");
+});
+
+var button2 = document.getElementById("button2");
+button2.addEventListener("click", function(){
+    console.log("clic");
+});
+
+var button3 = document.getElementById("button3");
+button3.addEventListener("click", function(){
+    console.log("clic");
+});
